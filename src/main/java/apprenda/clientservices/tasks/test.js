770bb90e2616da17c7717577262da0642c9dcda9
Jason van Brackel <jvanbrackel@apprenda.com>
2016-02-23 07:19:38 -0500

    Update documentation images

---
+++ src/main/java/apprenda/clientservices/tasks/test.js
@@ -0,0 +1,46 @@
+{
+    "success":true,
+    "sections":
+    [{"title": "Archive Upload Results", "success": true, "messages": []}, {
+        "title": "Promotion Prerequisites",
+        "success": true,
+        "messages": [{"severity": "Information", "message": "Created promotion tasks."}]
+    }, {"title": "Updating version stage", "success": true, "messages": []}, {
+        "title": "Discovering WCF Topology",
+        "success": true,
+        "messages": []
+    }, {
+        "title": "Creating Version Metadata",
+        "success": true,
+        "messages": []
+    }, {
+        "title": "Create Application Data Partition",
+        "success": true,
+        "messages": [{"severity": "Information", "message": "Completed creation of data model"}]
+    }, {"title": "Staging WCF Services", "success": true, "messages": []}, {
+        "title": "Configure Application Tenant",
+        "success": true,
+        "messages": [{"severity": "Information", "message": "Successfully created application tenant"}]
+    }, {
+        "title": "Onboard Application Tenant to Application",
+        "success": true,
+        "messages": [{"severity": "Information", "message": "Application Tenant Onboarded"}]
+    }, {
+        "title": "Ensure Minimum Instance Counts",
+        "success": true,
+        "messages": [{
+            "severity": "Information",
+            "message": "Files for logpumper exist and are properly configured."
+        }, {
+            "severity": "Information",
+            "message": "Sites for logpumper exist and are properly configured."
+        }, {"severity": "Information", "message": "Successfully deployed 1 partition."}, {
+            "severity": "Information",
+            "message": "Components deployed to meet required instance counts"
+        }]
+    }, {"title": "Prepare State Flip", "success": true, "messages": []}, {
+        "title": "Commit State Flip",
+        "success": true,
+        "messages": []
+    }, {"title": "Completing Promotion", "success": true, "messages": []}]
+}
